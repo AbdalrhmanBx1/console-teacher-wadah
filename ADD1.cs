c66f043be4d40e61ed09d979c6f473ae0be8bf59
AbdalrhmanBx1 <126502851+AbdalrhmanBx1@users.noreply.github.com>
2023-02-27 21:09:49 +0300

    console

---
+++ ADD1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Polymorephism1
+{
+    class ADD1
+    {
+        public void add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void add(float x, float y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public void add(float a, float b, float c)
+        {
+            Console.WriteLine(a + b + c);
+        }
+        public void add(string s1, string s2)
+        {
+            Console.WriteLine(s1 + "\t" + s2);
+        }
+    }
+    class ADD2:ADD1
+    {
+        public void add(string s1, string s2, string s3)
+        {
+            Console.WriteLine(s1 + "\t" + s2 + "\t" + s3);
+        }
+
+    }
+}
